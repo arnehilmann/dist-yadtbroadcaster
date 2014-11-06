4aebf631d09cf2655f83655ba89304b13fdee7fd
Arne Hilmann <arne@hilmann.de>
2014-11-06 16:08:27 +0100

    send forwarded messages to own peers

--- src/dist_wamp_router.erl
+++ src/dist_wamp_router.erl
@@ -69,7 +69,22 @@ listen_for_forwards() ->
     receive
         {From, Realm, Data} ->
             io:format("received data from ~p on realm ~p:~n~p~n", [From, Realm, Data]),
-            gproc:send({p, l, {router, Realm}}, Data)
+            forward_message(Data, Realm)
     end,
     listen_for_forwards().
 
+forward_message(Message, Realm) when is_bitstring(Realm) ->
+  case erwa_realms:get_router(Realm) of
+    {ok,Pid} ->
+      forward_message(Message,Pid);
+    {error,not_found} ->
+      self() ! {erwa,{abort,[{}],no_such_realm}},
+      self() ! {erwa, shutdown},
+      {error,undefined}
+  end;
+forward_message(Message, Router) when is_pid(Router) ->
+  Router ! {forwarded, Message},
+  ok;
+forward_message(AnyData, AnyObject) ->
+    io:format("cannot handle realm/router ~p~ndropping data ~p~n", [AnyObject, AnyData]),
+    ok.
