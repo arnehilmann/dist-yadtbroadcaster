508899945a58aa4b7a9e53d9298cfa54b6a5c44d
mriehl <max@riehl.io>
2014-11-20 14:41:45 +0100

    pack init script with RPM

--- build_rpm_with_fpm.sh
+++ build_rpm_with_fpm.sh
@@ -4,4 +4,4 @@ MINOR_VERSION=${1:?no MINOR_VERSION specified}
 
 cp -r _rel dist-wamp-router
 
-fpm -t rpm -v 1.$MINOR_VERSION --rpm-auto-add-directories --directories /opt/dist-wamp-router --rpm-group admins --rpm-use-file-permissions  --rpm-defattrdir 0775 -n dist-wamp-router -s dir dist-wamp-router=/opt/
+fpm -t rpm -v 1.$MINOR_VERSION --rpm-auto-add-directories --directories /opt/dist-wamp-router --rpm-group admins --rpm-use-file-permissions  --rpm-defattrdir 0775 -n dist-wamp-router -s dir dist-wamp-router=/opt/ res/initscript/dist-wamp-router=/etc/init.d/dist-wamp-router
