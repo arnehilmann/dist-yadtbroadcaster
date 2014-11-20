8575746765b005e7dabfc5f43ba3ef6c0dceee97
mriehl <max@riehl.io>
2014-11-20 14:39:06 +0100

    add rpm build script

---
+++ build_rpm_with_fpm.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+MINOR_VERSION=${1:?no MINOR_VERSION specified}
+
+cp -r _rel dist-wamp-router
+
+fpm -t rpm -v 1.$MINOR_VERSION --rpm-auto-add-directories --directories /opt/dist-wamp-router --rpm-group admins --rpm-use-file-permissions  --rpm-defattrdir 0775 -n dist-wamp-router -s dir dist-wamp-router=/opt/
