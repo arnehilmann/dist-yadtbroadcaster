6bb07f7b693d3efbdae5f3d4c771fe129f156767
mriehl <max@riehl.io>
2014-11-21 11:29:18 +0100

    package yadt service snippet with rpm

--- build_rpm_with_fpm.sh
+++ build_rpm_with_fpm.sh
@@ -9,4 +9,4 @@ MINOR_VERSION=${1:?no MINOR_VERSION specified, usage is $0 MINOR_VERSION}
 
 cp -r _rel dist-wamp-router
 
-fpm -t rpm -v 1.$MINOR_VERSION --directories /opt/dist-wamp-router --depends riak --after-install res/postinstall/chkconfig_dist_wamp_router.sh --after-remove res/postuninstall/un-chkconfig_dist_wamp_router.sh --rpm-group admins --rpm-use-file-permissions --rpm-defattrdir 0775 -n dist-wamp-router -s dir dist-wamp-router=/opt/ res/initscript/dist-wamp-router=/etc/init.d/dist-wamp-router
+fpm -t rpm -v 1.$MINOR_VERSION --directories /opt/dist-wamp-router --depends riak --after-install res/postinstall/chkconfig_dist_wamp_router.sh --after-remove res/postuninstall/un-chkconfig_dist_wamp_router.sh --rpm-group admins --rpm-use-file-permissions --rpm-defattrdir 0775 -n dist-wamp-router -s dir dist-wamp-router=/opt/ res/initscript/dist-wamp-router=/etc/init.d/dist-wamp-router res/yadt/dist_wamp_router.yaml=/etc/yadt.conf.d/dist_wamp_router.yaml
