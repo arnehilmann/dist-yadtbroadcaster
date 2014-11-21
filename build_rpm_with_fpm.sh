22b7957c2f19bdf77698013c0043ae5e04347c20
mriehl <max@riehl.io>
2014-11-21 09:09:46 +0100

    require riak by rpm dependency

--- build_rpm_with_fpm.sh
+++ build_rpm_with_fpm.sh
@@ -4,4 +4,4 @@ MINOR_VERSION=${1:?no MINOR_VERSION specified}
 
 cp -r _rel dist-wamp-router
 
-fpm -t rpm -v 1.$MINOR_VERSION --directories /opt/dist-wamp-router --after-install res/postinstall/chkconfig_dist_wamp_router.sh --after-remove res/postuninstall/un-chkconfig_dist_wamp_router.sh --rpm-group admins --rpm-use-file-permissions --rpm-defattrdir 0775 -n dist-wamp-router -s dir dist-wamp-router=/opt/ res/initscript/dist-wamp-router=/etc/init.d/dist-wamp-router
+fpm -t rpm -v 1.$MINOR_VERSION --directories /opt/dist-wamp-router --depends riak --after-install res/postinstall/chkconfig_dist_wamp_router.sh --after-remove res/postuninstall/un-chkconfig_dist_wamp_router.sh --rpm-group admins --rpm-use-file-permissions --rpm-defattrdir 0775 -n dist-wamp-router -s dir dist-wamp-router=/opt/ res/initscript/dist-wamp-router=/etc/init.d/dist-wamp-router
