18a8f1cb12eea9c8d4c8839aae667786af36436f
Arne Hilmann <arne@hilmann.de>
2014-12-15 17:07:51 +0100

    return combined broadcaster/riak status in body

--- src/status_handler.erl
+++ src/status_handler.erl
@@ -4,17 +4,24 @@
 -export([handle/2]).
 -export([terminate/3]).
 
+-define(RIAK_STATS_URL, "http://localhost:8098/stats").
+
 init(_Transport, Req, []) ->
     {ok, Req, undefined}.
 
 handle(Req, State) ->
+    {ok, {Status, _Header, Body}} = httpc:request(?RIAK_STATS_URL),
+    {_HttpVersion, Code, _Text} = Status,
+    CompleteBody = "{\"broadcast-status\": \"ok\", \"riak-status\": " ++ Body ++ "}",
+    PrettyBody = jsx:prettify(list_to_binary(CompleteBody)),
     cowboy_req:reply(
-      200,
+      Code,
       [ {<<"content-type">>, <<"text/plain">>} ],
-      <<"ok">>,
+      PrettyBody,
       Req
      ),
     {ok, Req, State}.
 
 terminate(_Reason, _Req, _State) ->
     ok.
+
