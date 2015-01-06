5805e5acbd5d4d9e1761bc334f4839f53772baa9
Arne Hilmann <arne@hilmann.de>
2015-01-06 20:19:15 +0100

    report nodename and connected nodes in broadcast-status

--- src/status_handler.erl
+++ src/status_handler.erl
@@ -10,9 +10,11 @@ init(_Transport, Req, []) ->
     {ok, Req, undefined}.
 
 handle(Req, State) ->
-    {ok, {Status, _Header, Body}} = httpc:request(?RIAK_STATS_URL),
+    {ok, {Status, _Header, RiakStatus}} = httpc:request(?RIAK_STATS_URL),
     {_HttpVersion, Code, _Text} = Status,
-    CompleteBody = "{\"broadcast-status\": \"ok\", \"riak-status\": " ++ Body ++ "}",
+    NodeName = io_lib:format("~s", [node()]),
+    DycStatus = "{\"nodename\": \"" ++ NodeName ++ "\", \"connected_nodes\": " ++ io_lib:format("~p", [nodes(connected)]) ++ ", \"make_monitoring_happy\": \"ok\"}",
+    CompleteBody = "{\"broadcast-status\": " ++ DycStatus ++ ", \"riak-status\": " ++ RiakStatus ++ "}",
     PrettyBody = jsx:prettify(list_to_binary(CompleteBody)),
     cowboy_req:reply(
       Code,
